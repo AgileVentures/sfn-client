2f2c81d31017cc941ca4df96b1384bbe250d8d4e
aonomike <aonomike@gmail.com>
2019-01-12 22:36:42 +0300

    Move App component to components directory

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ArtistContainer from './ArtistContainer'
+const App = () => {
+    return <div><ArtistContainer /></div>
+  };
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const App = () => {
-  return <div>App</div>;
-};
+import App from './components/App'
 
 ReactDOM.render(<App />, document.querySelector("#root"));
