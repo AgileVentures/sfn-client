7fa69799685f1367bddef222363b765c09c4d5b5
Peter Javorkai <javorkai.peter@gmail.com>
2019-01-21 20:29:35 +0100

    Added missing routes

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,11 @@ import {BrowserRouter, Route} from "react-router-dom";
 import HomeContainer from "./Home/HomeContainer";
 import AboutContainer from "./About/AboutContainer";
 import CausesContainer from "./Causes/CausesContainer";
+import CauseContainer from "./Cause/CauseContainer";
+import ArtistsContainer from "./Artists/ArtistsContainer";
 import ArtistContainer from "./Artist/ArtistContainer";
 import PerformancesContainer from "./Performances/PerformancesContainer";
+import PerformanceContainer from "./Performance/PerformanceContainer";
 import ContactContainer from "./Contact/ContactContainer";
 
 const App = () => {
@@ -17,8 +20,11 @@ const App = () => {
           <Route path="/" exact component={HomeContainer} />
           <Route path="/about" component={AboutContainer} />
           <Route path="/causes" component={CausesContainer} />
+          <Route path="/causes/:id" component={CauseContainer} />
+          <Route path="/artists/:" component={ArtistsContainer} />
           <Route path="/artists/:id" component={ArtistContainer} />
           <Route path="/performances" component={PerformancesContainer} />
+          <Route path="/performances/:id" component={PerformanceContainer} />
           <Route path="/contact" component={ContactContainer} />
         </div>
       </BrowserRouter>
