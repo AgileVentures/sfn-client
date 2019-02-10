218c038ff0adefe036ab6ce2fe396cc13487427e
Peter Javorkai <javorkai.peter@gmail.com>
2019-02-10 22:50:17 +0100

    Added perfomance component

--- src/components/Artist/ArtistPerformanceList/ArtistPerformanceList.js
+++ src/components/Artist/ArtistPerformanceList/ArtistPerformanceList.js
@@ -1,10 +1,14 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import ArtistPerformance from "../ArtistPerformance/ArtistPerformance";
 
 class ArtistPerformanceList extends Component {
   render() {
     return (
       <div className="artist-performance-container">
-        <h3 className="white">Performance</h3>
+        <h3 className="white">Performances</h3>
+        <ArtistPerformance />
+        <ArtistPerformance />
+        <ArtistPerformance />
       </div>
     );
   }
