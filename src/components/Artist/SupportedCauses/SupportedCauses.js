c079696bcfa40e85a8c05d9dfee52065ccf3ce07
aonomike <aonomike@gmail.com>
2019-02-17 00:34:03 +0300

    Add SupportedCauses component

---
+++ src/components/Artist/SupportedCauses/SupportedCauses.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SupportedCauses = () => {
+    return (
+        <div> Supported Causes </div>
+    );
+}
+
+export default SupportedCauses;
---
+++ src/components/Artist/SupportedCauses/SupportedCauses.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import SupportedCauses from './SupportedCauses'
+
+it ("creates a component without crashing", () => {
+    const supportedCause = <SupportedCauses/>;
+});
