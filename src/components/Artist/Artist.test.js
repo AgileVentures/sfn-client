fe71e7f5dba522b63d04857f6282b7e526378bb4
Peter Javorkai <javorkai.peter@gmail.com>
2019-03-02 19:42:28 +0100

    Added enzyme test for Artist

--- src/components/Artist/Artist.test.js
+++ src/components/Artist/Artist.test.js
@@ -1,9 +1,25 @@
 import React from "react";
 import Artist from "./ArtistContainer";
+import ArtistBio from "./ArtistBio"
+import MoneyRaised from "./MoneyRaised/MoneyRaised"
+import {
+  shallow
+} from "enzyme"
+
+let wrapped;
 
 //Need at least 1 test to pass Travis CI
-describe ("<ArtistContainer />", () => {
-  it ("creates component without crashing", () => {
-    const artist = <Artist/>;
-  });
+describe("<ArtistContainer />", () => {
+
+  beforeEach(() => {
+    wrapped = shallow( < Artist / > )
+  })
+
+  it("displays an artist bio", () => {
+    expect(wrapped.find(ArtistBio).length).toEqual(1)
+  })
+
+  it("displays an money raised", () => {
+    expect(wrapped.find(MoneyRaised).length).toEqual(1)
+  })
 });
