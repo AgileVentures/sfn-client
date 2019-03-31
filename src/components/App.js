1df988afe9c1f5f74a40dbe29f957afd5590bda8
Peter Javorkai <javorkai.peter@gmail.com>
2019-03-31 20:39:07 +0200

    Added footer component

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,7 @@ import Performances from "../containers/Performances/Performances";
 import PerformanceContainer from "./Performance/PerformanceContainer";
 import ContactContainer from "./Contact/ContactContainer";
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 const App = () => {
   return (
@@ -32,6 +33,7 @@ const App = () => {
           <Route path="/performances/:id" component={PerformanceContainer} />
           <Route path="/contact" component={ContactContainer} />
         </main>
+        <Footer />
       </div>
     </BrowserRouter>
   );
