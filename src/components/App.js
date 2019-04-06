ffc35548e869b5df2f644749b9268807ee21098d
Federico Esparza <federicoesparza@outlook.com>
2019-04-06 12:01:27 -0500

    Remove cause, home, and performance components, modify app paths

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ import {BrowserRouter, Route} from "react-router-dom";
 import "../main.scss";
 
 // Components
-import HomeContainer from "./Home/HomeContainer";
+import Home from "../containers/Home/Home";
 import About from "../containers/About/About";
 import Causes from "../containers/Causes/Causes";
-import CauseContainer from "./Cause/CauseContainer";
+import Cause from "../containers/Cause/Cause";
 import Artists from "../containers/Artists/Artists";
 import Artist from "../containers/Artist/Artist"
 import Performances from "../containers/Performances/Performances";
-import PerformanceContainer from "./Performance/PerformanceContainer";
+import Performance from "../containers/Performance/Performance";
 import Contact from "../containers/Contact/Contact";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
@@ -23,14 +23,14 @@ const App = () => {
       <div>
         <Header />
         <main className="main-container grid">
-          <Route path="/" exact component={HomeContainer} />
+          <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/causes" exact component={Causes} />
-          <Route path="/causes/:id" component={CauseContainer} />
+          <Route path="/causes/:id" component={Cause} />
           <Route path="/artists" exact component={Artists} />
           <Route path="/artists/:id" component={Artist} />
           <Route path="/performances" exact component={Performances} />
-          <Route path="/performances/:id" component={PerformanceContainer} />
+          <Route path="/performances/:id" component={Performance} />
           <Route path="/contact" component={Contact} />
         </main>
         <Footer />
