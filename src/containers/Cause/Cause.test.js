49683832c3a9ef87338c0a9dc74dfcb3e8b640a6
Federico Esparza <federicoesparza@outlook.com>
2019-04-06 12:00:12 -0500

    Move Cause to containers folder

---
+++ src/containers/Cause/Cause.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Cause from "./Cause";
+import {
+  shallow
+} from 'enzyme'
+
+//Need at least 1 test to pass Travis CI
+describe("<Cause />", () => {
+
+  it('creates component without crashing', () => {
+    const cause = < Cause / >
+  })
+
+  it('contains text Cause', () => {
+    const causeWrapper = shallow( < Cause / > )
+    expect(causeWrapper.text()).toEqual('Cause')
+  })
+});
