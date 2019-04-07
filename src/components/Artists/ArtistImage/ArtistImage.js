276eaa7779b653860d9b333bfa42635f90aa22f6
aonomike <aonomike@gmail.com>
2019-04-07 23:16:19 +0300

    Style ArtistImage

--- src/components/Artists/ArtistImage/ArtistImage.js
+++ src/components/Artists/ArtistImage/ArtistImage.js
@@ -2,12 +2,16 @@ import React from "react";
 
 const ArtistImage =() => {
     return (<div className="artist-details-container">
-        <img
-        className="artist-details__avatar"
-        src="/images/dua_lipa_profile.jpg"
-        alt="Dua Lipa profile"
-        />
-        <h5 className="artist-details__name white">Dua Lipa</h5>
+        <div className="artist-image">
+            <img
+                className="artist-details__avatar"
+                src="/images/dua_lipa_profile.jpg"
+                alt="Dua Lipa profile"
+                />
+        </div>
+        <div className="artist-name">
+            <h5 className="artist-details__name white">Dua Lipa</h5>
+        </div>
     </div>);
 }
 export default ArtistImage;
