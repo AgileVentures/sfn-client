5dde55a49a4e7c77c0f0732755198b4e0e065b24
aonomike <aonomike@gmail.com>
2019-04-07 23:51:40 +0300

    Make Artist name appear below Image

--- src/components/Artists/ArtistImage/ArtistImage.js
+++ src/components/Artists/ArtistImage/ArtistImage.js
@@ -1,17 +1,13 @@
 import React from "react";
 
 const ArtistImage =() => {
-    return (<div className="artist-details-container">
-        <div className="artist-image">
-            <img
-                className="artist-details__avatar"
-                src="/images/dua_lipa_profile.jpg"
-                alt="Dua Lipa profile"
-                />
-        </div>
-        <div className="artist-name">
-            <h5 className="artist-details__name white">Dua Lipa</h5>
-        </div>
+    return (<div className="artist-image">
+        <img
+        className="artist-image__avatar"
+        src="/images/dua_lipa_profile.jpg"
+        alt="Dua Lipa profile"
+        />
+        <h5 className="artist-image__name white">Dua Lipa</h5>
     </div>);
 }
 export default ArtistImage;
