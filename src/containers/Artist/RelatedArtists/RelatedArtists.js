dc6a2c4ee25543c8fe8cef18b59d62639d36e332
aonomike <aonomike@gmail.com>
2019-04-13 22:29:48 +0300

    Add list of related Artist and Supported cause

--- src/containers/Artist/RelatedArtists/RelatedArtists.js
+++ src/containers/Artist/RelatedArtists/RelatedArtists.js
@@ -4,6 +4,23 @@ const RelatedArtists = () => {
   return (
     <div className="relatedartists-container">
       <h3 className="white">RelatedArtists</h3>
+      <div>
+      <ul className="related-artist-item-list white">
+        <li>
+          Awesome Artist A
+        </li>
+        <li>
+          Awesome Artist B
+        </li>
+        <li>
+          Awesome Artist C
+        </li>
+        <li>
+          Awesome Artist D
+        </li>
+
+      </ul>
+      </div>
     </div>
   );
 };
--- src/containers/Artist/SupportedCauses/SupportedCauses.js
+++ src/containers/Artist/SupportedCauses/SupportedCauses.js
@@ -4,6 +4,23 @@ const SupportedCauses = () => {
   return (
     <div className="supportedcauses-container">
       <h3 className="white">Supported Causes</h3>
+      <div>
+      <ul className="supported-causes-item-list white">
+        <li>
+          Deserving Cause A
+        </li>
+        <li>
+          Deserving Cause B
+        </li>
+        <li>
+          Deserving Cause C
+        </li>
+        <li>
+          Deserving Cause D
+        </li>
+
+      </ul>
+      </div>
     </div>
   );
 };
