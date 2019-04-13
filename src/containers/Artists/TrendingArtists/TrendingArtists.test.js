e581a44f89cf55b37439030f80072a1c13e3569d
aonomike <aonomike@gmail.com>
2019-04-13 21:10:47 +0300

    Add test around Trending Artist page title

--- src/containers/Artists/TrendingArtists/TrendingArtists.test.js
+++ src/containers/Artists/TrendingArtists/TrendingArtists.test.js
@@ -3,8 +3,15 @@ import TrendingArtists from "./TrendingArtists";
 import { shallow } from "enzyme";
 
 describe("<TrendingArtists />", () => {
+  let  TrendingArtistsWrapper
+  beforeEach(()=>{
+    TrendingArtistsWrapper = shallow(<TrendingArtists />);
+  });
   it("Has 4 Trending Artists", () => {
-    const TrendingArtistsWrapper = shallow(<TrendingArtists />);
+    
     expect(TrendingArtistsWrapper.find('TrendingArtist').length).toEqual(4);
+  })
+  it("has 'Trending Artists' title", ()=>{
+    expect(TrendingArtistsWrapper.find('h1').text()).toEqual("Trending Artists")
   })
 });
