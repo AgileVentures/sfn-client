70531649673f0e2ca6225b1d09c3e050a0815f8e
jmpainter <jpainter4@gmail.com>
2019-04-13 21:55:16 -0700

    Added basic layout for TrendingArtist component

--- src/components/Artists/ArtistImage/ArtistImage.js
+++ src/components/Artists/ArtistImage/ArtistImage.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
 
-const ArtistImage =() => {
-    return (<div className="artist-image">
-        <img
+const ArtistImage = () => {
+  return (
+    <div className="artist-image">
+      <img
         className="artist-image__avatar"
         src="/images/dua_lipa_profile.jpg"
         alt="Dua Lipa profile"
-        />
-        <h5 className="artist-image__name white">Dua Lipa</h5>
-    </div>);
-}
+      />
+    </div>
+  );
+};
 export default ArtistImage;
--- src/containers/Artists/TrendingArtists/TrendingArtists.js
+++ src/containers/Artists/TrendingArtists/TrendingArtists.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
-import TrendingArtist from '../../../components/Artists/TrendingArtist/TrendingArtist'
+import React, { Component } from 'react';
+import TrendingArtist from '../../../components/Artists/TrendingArtist/TrendingArtist';
 
 class TrendingArtists extends Component {
   render() {
     return (
-      <div className="trending-artists-container">
+      <React.Fragment>
         <h1 className="trending-artists-title">Trending Artists</h1>
-        <div className='trending-artists'>
-          < TrendingArtist />
-          < TrendingArtist />
-          < TrendingArtist />
-          < TrendingArtist />
-        </div>
-      </div>
-      );
+        <TrendingArtist />
+        <TrendingArtist />
+        <TrendingArtist />
+        <TrendingArtist />
+      </React.Fragment>
+    );
   }
 }
 
