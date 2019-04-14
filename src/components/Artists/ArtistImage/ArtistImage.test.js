6a8bbb0a850c15ed8fcee659262fa1e42adb0ff6
jmpainter <jpainter4@gmail.com>
2019-04-14 08:17:16 -0700

    Updated ArtistImage.test to remove test for artist name

--- src/components/Artists/ArtistImage/ArtistImage.test.js
+++ src/components/Artists/ArtistImage/ArtistImage.test.js
@@ -1,22 +1,15 @@
-import React from 'react'
-import ArtistImage from './ArtistImage'
+import React from 'react';
+import ArtistImage from './ArtistImage';
 
-import {
-  shallow
-} from 'enzyme'
+import { shallow } from 'enzyme';
 
 describe('ArtistImage', () => {
-
-  let artistImageWrapper
+  let artistImageWrapper;
   beforeEach(() => {
-    artistImageWrapper = shallow( < ArtistImage/> )
-  })
+    artistImageWrapper = shallow(<ArtistImage />);
+  });
 
   it('has an image of the artist', () => {
-    expect(artistImageWrapper.find('img').length).toEqual(1)
-  })
-
-  it('has the artist name', () => {
-      expect(artistImageWrapper.find('h5').text()).toEqual('Dua Lipa')
-  })
-})
+    expect(artistImageWrapper.find('img').length).toEqual(1);
+  });
+});
