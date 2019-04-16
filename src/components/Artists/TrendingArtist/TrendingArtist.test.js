af8a15e8a7bdb4a4ef9e8244a6059d777aee4430
jmpainter <jpainter4@gmail.com>
2019-04-16 13:22:41 -0700

    added more tests for TrendingArtist component

--- src/components/Artists/TrendingArtist/TrendingArtist.test.js
+++ src/components/Artists/TrendingArtist/TrendingArtist.test.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import TrendingArtist from './TrendingArtist'
+import React from 'react';
+import TrendingArtist from './TrendingArtist';
 
-import {
-  shallow
-} from 'enzyme'
+import { shallow } from 'enzyme';
 
 describe('TrendingArtist', () => {
+  let wrapper;
 
-  it('has text TrendingArtist', () => {
-    const trendingArtistWrapper = shallow( < TrendingArtist/> )
-    expect(trendingArtistWrapper.find('ArtistImage').length).toEqual(1)
-  })
-})
+  beforeEach(() => {
+    wrapper = shallow(<TrendingArtist />);
+  });
+
+  it('contains an ArtistImage', () => {
+    expect(wrapper.find('ArtistImage').length).toEqual(1);
+  });
+
+  it('contains the artist name', () => {
+    expect(wrapper.find('p.trending-artist-name').length).toEqual(1);
+  });
+
+  it('contains the artist detail', () => {
+    expect(wrapper.find('div.trending-artist-detail').length).toEqual(1);
+  });
+
+  it('contains the funds raised', () => {
+    expect(wrapper.find('p.trending-artist-funds').length).toEqual(1);
+  });
+
+  it('contains a Learn More button', () => {
+    expect(wrapper.contains(<button>Learn More</button>)).toEqual(true);
+  });
+});
