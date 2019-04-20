f58b1fe669958e09f3ac5e1418181010fc835b75
Peter Javorkai <javorkai.peter@gmail.com>
2019-04-20 20:52:51 +0200

    Added Explore Artists container

--- src/containers/Artists/Artists.js
+++ src/containers/Artists/Artists.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import TrendingArtists from './TrendingArtists/TrendingArtists'
+import ExploreArtists from './ExploreArtists/ExploreArtists'
 
 class Artists extends Component {
   render() {
-    return <TrendingArtists />
+    return (
+      <React.Fragment>
+        <TrendingArtists />
+        <ExploreArtists />
+      </React.Fragment>
+    )
   }
 }
 
