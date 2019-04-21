67fc6183fbfd6299cfcba33d47626981aed62430
Peter Javorkai <javorkai.peter@gmail.com>
2019-04-21 19:50:25 +0200

    Added missing test to Artists container about checking the Explore Artists container

--- src/containers/Artists/Artists.test.js
+++ src/containers/Artists/Artists.test.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Artists from './Artists'
-import { shallow } from 'enzyme'
+import {
+  shallow
+} from 'enzyme'
 
 describe('<Artists />', () => {
+  let artistsWrapper
+  beforeEach(() => {
+    artistsWrapper = shallow(<Artists />)
+  })
   it('contains 1 Trending Artists container', () => {
-    const artistsWrapper = shallow(<Artists />)
     expect(artistsWrapper.find('TrendingArtists').length).toEqual(1)
+  })
+
+  it('contains 1 Explore Artists container', () => {
+    expect(artistsWrapper.find('ExploreArtists').length).toEqual(1)
   })
 })
