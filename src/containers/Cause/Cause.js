2d226b0c300fd152f079e2c972e73a1582c107ed
aonomike <aonomike@gmail.com>
2019-04-27 22:44:30 +0300

    Update Cause Container and maim.scss with Cause containers and components
    Co-authored-by: Dan Hemmingson <danhemm@gmail.com>
    Co-authored-by: Peter Javorkai <javorkai.peter@gmail.com>

--- src/containers/Cause/Cause.js
+++ src/containers/Cause/Cause.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import CauseBio from '../../components/Cause/CauseBio/CauseBio'
 import Donate from '../../components/Cause/Donate/Donate'
 import Campaign from '../../components/Cause/Campaign/Campaign'
+import ContactUs from '../../components/Cause/ContactUs/ContactUs'
+import SupportingArtists from './SupportingArtists/SupportingArtists'
+import RelatedCauses from './RelatedCauses/RelatedCauses'
+import RecentDonors from './RecentDonors/RecentDonors'
 
 class Cause extends Component {
   render() {
@@ -10,6 +14,10 @@ class Cause extends Component {
         <CauseBio />
         <Donate />
         <Campaign />
+        <ContactUs />
+        <SupportingArtists />
+        <RelatedCauses />
+        <RecentDonors />
       </div>
     )
   }
--- src/containers/Cause/Cause.test.js
+++ src/containers/Cause/Cause.test.js
@@ -13,4 +13,28 @@ describe('<Cause />', () => {
   it('contains CauseBio component', () => {
     expect(causeWrapper.find('CauseBio').length).toEqual(1)
   })
+
+  it('contains Donate component', () => {
+    expect(causeWrapper.find('Donate').length).toEqual(1)
+  })
+
+  it('contains Campaign component', () => {
+    expect(causeWrapper.find('Campaign').length).toEqual(1)
+  })
+
+  it('contains ContactUs component', () => {
+    expect(causeWrapper.find('ContactUs').length).toEqual(1)
+  })
+
+  it('contains SupportingArtists component', () => {
+    expect(causeWrapper.find('SupportingArtists').length).toEqual(1)
+  })
+
+  it('contains RelatedCauses component', () => {
+    expect(causeWrapper.find('RelatedCauses').length).toEqual(1)
+  })
+
+  it('contains RecentDonors component', () => {
+    expect(causeWrapper.find('RecentDonors').length).toEqual(1)
+  })
 })
