d605bb0a2cf817b0bf79afa8ac3f485fbe122e00
aonomike <aonomike@gmail.com>
2019-04-27 22:43:12 +0300

    Add Cause/SupportingArtists container

---
+++ src/containers/Cause/SupportingArtists/SupportingArtists.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class SupportingArtists extends Component {
+  render() {
+    return (
+      <div>SupportingArtists</div>
+    )
+  }
+}
+
+export default SupportingArtists
