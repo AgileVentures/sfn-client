6ad13f3dbf5fd32d60cffb9300f9083fef1d3c5b
Peter Javorkai <javorkai.peter@gmail.com>
2019-04-27 19:39:57 +0200

    Banner test refactored and added test to Artists container to test banner

--- src/components/Artists/Banner/Banner.test.js
+++ src/components/Artists/Banner/Banner.test.js
@@ -4,18 +4,21 @@ import Banner from './Banner'
 import { shallow } from 'enzyme'
 
 describe('Banner', () => {
+  let bannerWrapper
+
+  beforeEach(() => {
+    bannerWrapper = shallow(<Banner />)
+  })
+
   it('has a sign up button', () => {
-    const bannerWrapper = shallow(<Banner />)
     expect(bannerWrapper.find('button').length).toEqual(1)
   })
 
   it('has a headline defined', () => {
-    const bannerWrapper = shallow(<Banner />)
     expect(bannerWrapper.find('h3').length).toEqual(1)
   })
 
   it('has a paragraph', () => {
-    const bannerWrapper = shallow(<Banner />)
     expect(bannerWrapper.find('p').length).toEqual(1)
   })
 })
