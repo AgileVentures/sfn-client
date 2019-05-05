f8267f8c7c1ce2863b98df824ea171dbeb50db03
aonomike <aonomike@gmail.com>
2019-05-05 23:31:58 +0300

    Add margin to the paragraphs

--- src/components/Cause/CauseBio/CauseBioDetail.js
+++ src/components/Cause/CauseBio/CauseBioDetail.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types'
 const CauseBioDetail = props => {
   return (
     <div className="cause-bio-detail-container">
-      <h5 className="white">{props.heading}</h5>
-      <p className="white">{props.content}</p>
+      <h5 className="white cause-bio-detail__heading">{props.heading}</h5>
+      <p className="white cause-bio-detail__content">{props.content}</p>
     </div>
   )
 }
