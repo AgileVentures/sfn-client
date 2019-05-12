2db14b9a4d8feb34ea67d6422c252713a2106447
Peter Javorkai <javorkai.peter@gmail.com>
2019-05-12 20:57:28 +0200

    Fixed alignment issues on causes page and added links that were missing underneath the cards

--- src/containers/Causes/TrendingCauses/TrendingCauses.test.js
+++ src/containers/Causes/TrendingCauses/TrendingCauses.test.js
@@ -9,6 +9,14 @@ describe('<TrendingArtists />', () => {
   })
 
   it("has 'Trending Causes' title", () => {
-    expect(trendingCausesWrapper.find('h2').text()).toEqual('Trending Causes')
+    expect(trendingCausesWrapper.find('h3').text()).toEqual('Trending causes')
+  })
+
+  it('has 2 trending cards showing underneath', () => {
+    expect(trendingCausesWrapper.find('CauseCard').length).toEqual(2)
+  })
+
+  it('has link to navigate to all trending causes', () => {
+    expect(trendingCausesWrapper.find('NavLink').length).toEqual(1)
   })
 })
