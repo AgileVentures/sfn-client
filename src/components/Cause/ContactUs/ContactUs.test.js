2c9a23e125e2b4e8ca57f4425fa1479127353654
Peter Javorkai <javorkai.peter@gmail.com>
2019-05-18 21:29:54 +0200

    Added additional Font awesome libraries for socials and created the contact us component

--- src/components/Cause/ContactUs/ContactUs.test.js
+++ src/components/Cause/ContactUs/ContactUs.test.js
@@ -10,7 +10,25 @@ describe('ContactUs', () => {
     contactUsWrapper = shallow(<ContactUs />)
   })
 
-  it('has text ContactUs', () => {
-    expect(contactUsWrapper.find('div').text()).toEqual('ContactUs')
+  it('has title Contact us', () => {
+    expect(contactUsWrapper.find('h5.white').text()).toEqual('Contact us')
+  })
+
+  it('has the name of the charity spokesperson', () => {
+    expect(contactUsWrapper.find('h6').text()).toEqual(
+      'Mrs. Awesome Charity spokesperson'
+    )
+  })
+
+  it('has information about the contact details', () => {
+    expect(contactUsWrapper.find('div.contact-details--item').length).toEqual(
+      3
+    )
+  })
+
+  it('has social media channels for communication', () => {
+    expect(
+      contactUsWrapper.find('div.contact-details--socials').length
+    ).toEqual(1)
   })
 })
