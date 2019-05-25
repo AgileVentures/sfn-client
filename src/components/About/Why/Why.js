fc36bd1c5906fdc4f8f9c6ffd10d5f44f7589c20
Peter Javorkai <javorkai.peter@gmail.com>
2019-05-25 20:36:14 +0200

    Added why section to about page

---
+++ src/components/About/Why/Why.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Why = () => {
+  return (
+    <div className="why-container">
+      <h3 className="white">Why give with us?</h3>
+      <div className="why-description white">
+        <h5>Secure</h5>
+        <p>
+          We take online provacy and security seriously. All your information is
+          kept sound and safe.
+        </p>
+      </div>
+      <div className="why-description white">
+        <h5>Trusted</h5>
+        <p>
+          Sing for Needs is backed by the community around you. People across
+          the globe use Sing for Needs to make a difference. We are proud of
+          their trust in us.
+        </p>
+      </div>
+      <div className="why-description white">
+        <h5>Committed</h5>
+        <p>
+          Our services and platforms are free for the community. We don't charge
+          a fee. We're committed to our vision and goals to make a world a
+          better place!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Why
--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Vision from '../../components/About/Vision/Vision'
+import Why from '../../components/About/Why/Why'
 
 const About = () => {
   return (
     <div className="about-container">
       <Vision />
+      <Why />
     </div>
   )
 }
