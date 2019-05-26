e0f39736e57da2a84e8f988bb4931638a52a638b
Peter Javorkai <javorkai.peter@gmail.com>
2019-05-26 19:58:24 +0200

    Added how it works section

--- src/containers/About/About.test.js
+++ src/containers/About/About.test.js
@@ -9,7 +9,19 @@ describe('<About />', () => {
     aboutWrapper = shallow(<About />)
   })
 
-  it('has a vision component', () => {
+  it('has a vision component explaining what we focus on', () => {
     expect(aboutWrapper.find('Vision').length).toEqual(1)
+  })
+
+  it('has a why statement as the reason to give', () => {
+    expect(aboutWrapper.find('Why').length).toEqual(1)
+  })
+
+  it('has a how section with all the actors of the process', () => {
+    expect(aboutWrapper.find('How').length).toEqual(1)
+  })
+
+  it('has a sing up banner at the bottom to keep in touch', () => {
+    expect(aboutWrapper.find('Banner').length).toEqual(1)
   })
 })
