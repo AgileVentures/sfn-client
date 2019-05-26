6569404dcf894dff8f08693969b678fe8b5439c7
aonomike <aonomike@gmail.com>
2019-05-26 21:24:06 +0300

    Update Artist component test due to redux store
    - Start writing test for fetchArtist action

--- src/containers/Artists/Artists.test.js
+++ src/containers/Artists/Artists.test.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import Artists from './Artists'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import { Provider } from 'react-redux'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+let store
 
 describe('<Artists />', () => {
   let artistsWrapper
+  let props = {
+    fetchArtists: jest.fn()
+  }
+
   beforeEach(() => {
-    artistsWrapper = shallow(<Artists />)
+    artistsWrapper = mount(<Provider store={store}><Artists {...props} /></Provider>)
+    store = mockStore({})
   })
+
   it('contains 1 Trending Artists container', () => {
     expect(artistsWrapper.find('TrendingArtists').length).toEqual(1)
   })
