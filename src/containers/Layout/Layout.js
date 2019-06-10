b8c9b261c4a3635471dcdfa729c98b810b949f52
Peter Javorkai <javorkai.peter@gmail.com>
2019-06-10 19:37:29 +0200

    Closing mobile menu on body click

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -6,18 +6,36 @@ import PropTypes from 'prop-types'
 export default class Layout extends Component {
   state = {
     mobileMenu: false
+  };
+
+  componentDidMount() {
+    document.addEventListener('mouseup', this.handleClickOutside)
   }
+  componentWillUnmount() {
+    document.removeEventListener('mouseup', this.handleClickOutside)
+  }
+
+  handleClickOutside = () => {
+    if (this.state.mobileMenu) {
+      this.setState({
+        mobileMenu: false
+      })
+    }
+  };
 
   handleClick = () => {
     this.setState({
       mobileMenu: !this.state.mobileMenu
     })
-  }
+  };
 
   render() {
     return (
       <div>
-        <Header mobileMenu={this.state.mobileMenu} handleClick={this.handleClick} />
+        <Header
+          mobileMenu={this.state.mobileMenu}
+          handleClick={this.handleClick}
+        />
         {this.props.children}
         <Footer />
       </div>
