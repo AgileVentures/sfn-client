370b657309e3df207021ae90cb550ea98b71579a
aonomike <aonomike@gmail.com>
2019-06-22 21:04:52 +0300

    Update document title to Sing for Needs

--- cypress/integration/common/web_steps.js
+++ cypress/integration/common/web_steps.js
@@ -1,6 +1,13 @@
-import { Given } from 'cypress-cucumber-preprocessor/steps'
+import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
 
 const url = 'https://google.com'
 Given('I open Google page', () => {
   cy.visit(url)
+})
+
+Given('I visit the home page', () => {
+  cy.visit('/')
+})
+Then("I should see 'Sing For Needs' as document title", () => {
+  cy.title().should('contain', 'Sing for Needs')
 })
