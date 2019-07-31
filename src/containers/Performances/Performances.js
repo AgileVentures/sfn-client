2a3f75da83a8a6f1e55692504db73cbeb4c7f716
Peter Javorkai <javorkai.peter@gmail.com>
2019-07-31 20:18:38 +0200

    Fixing Travis errors

--- src/containers/Performances/Performances.js
+++ src/containers/Performances/Performances.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import TrendingPerformances from './TrendingPerformances/TrendingPerformances'
+import ExplorePerformances from './ExplorePerformances/ExplorePerformances'
+import Banner from '../../components/Shared/Banner/Banner'
 
 class Performances extends Component {
   render() {
     return (
       <React.Fragment>
         <TrendingPerformances />
+        <ExplorePerformances />
+        <Banner
+          headline="You can make a difference today"
+          buttonLabel="Sign up now"
+        />
       </React.Fragment>
     )
   }
--- src/containers/Performances/Performances.test.js
+++ src/containers/Performances/Performances.test.js
@@ -3,8 +3,22 @@ import Performances from './Performances'
 import { mount } from 'enzyme'
 
 describe('<Performances />', () => {
+  let performancesWrapper
+
+  beforeEach(() => {
+    performancesWrapper = mount(<Performances />)
+  })
+
   it('contains a Trending Performances wrapper', () => {
     const performancesWrapper = mount(<Performances />)
     expect(performancesWrapper.find('TrendingPerformances').length).toEqual(1)
+  })
+
+  it('contains an ExploreArtists section', () => {
+    expect(performancesWrapper.find('ExplorePerformances').length).toEqual(1)
+  })
+
+  it('contains a sign up banner at the bottom', () => {
+    expect(performancesWrapper.find('Banner').length).toEqual(1)
   })
 })
