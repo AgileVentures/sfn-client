56380fa681ae063cea3c801aa86a1a81946ed188
Peter Javorkai <javorkai.peter@gmail.com>
2019-09-29 19:46:49 +0200

    Footer is now responsive

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -4,57 +4,59 @@ import Logo from '../Logo/Logo'
 
 const Footer = () => {
   return (
-    <div className="footer__container">
+    <footer className="footer__container">
       <nav className="main-container footer__nav-container">
         <Logo />
-        <NavLink
-          className="nav_link"
-          exact
-          activeClassName="footer__nav__item--selected"
-          to="/"
-        >
-          Home
-        </NavLink>
-        <NavLink
-          className="nav_link"
-          activeClassName="footer__nav__item--selected"
-          to="/about"
-        >
-          About
-        </NavLink>
-        <NavLink
-          className="nav_link"
-          activeClassName="footer__nav__item--selected"
-          to="/causes"
-        >
-          Causes
-        </NavLink>
-        <NavLink
-          className="nav_link"
-          activeClassName="footer__nav__item--selected"
-          to="/artists"
-        >
-          Artists
-        </NavLink>
-        <NavLink
-          className="nav_link"
-          activeClassName="footer__nav__item--selected"
-          to="/performances"
-        >
-          Performances
-        </NavLink>
-        <NavLink
-          className="nav_link"
-          activeClassName="footer__nav__item--selected"
-          to="/contact"
-        >
-          Contact
-        </NavLink>
+        <div className="nav-links">
+          <NavLink
+            className="nav_link"
+            exact
+            activeClassName="footer__nav__item--selected"
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className="nav_link"
+            activeClassName="footer__nav__item--selected"
+            to="/about"
+          >
+            About
+          </NavLink>
+          <NavLink
+            className="nav_link"
+            activeClassName="footer__nav__item--selected"
+            to="/causes"
+          >
+            Causes
+          </NavLink>
+          <NavLink
+            className="nav_link"
+            activeClassName="footer__nav__item--selected"
+            to="/artists"
+          >
+            Artists
+          </NavLink>
+          <NavLink
+            className="nav_link"
+            activeClassName="footer__nav__item--selected"
+            to="/performances"
+          >
+            Performances
+          </NavLink>
+          <NavLink
+            className="nav_link"
+            activeClassName="footer__nav__item--selected"
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+        </div>
       </nav>
-      <h6 className="footer-container__copyright">
+      <h6 className="footer-container__copyright main-container">
         Copyright; 2019 Championer, Inc.
       </h6>
-    </div>
+    </footer>
   )
 }
 
