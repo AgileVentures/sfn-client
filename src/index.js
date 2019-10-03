8de58ed6cb824cbaa7e52e3806771a0207e2deca
aonomike <aonomike@gmail.com>
2019-10-03 18:50:55 +0300

    Add apollo client for querying graphql urls

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
 
 import App from './components/App'
 import configureStore from './store/store'
 
 const store = configureStore()
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
+const client = new ApolloClient({
+  url: process.env.REACT_APP_GQL_URL
+})
+ReactDOM.render(
+  <ApolloClient client={client}>
+    <Provider store={store}><App /></Provider>
+  </ApolloClient>, document.getElementById('root'))
