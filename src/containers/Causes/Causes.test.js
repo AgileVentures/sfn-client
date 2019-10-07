ca724c66b61eacd70d37eda124af2d47f0376779
aonomike <aonomike@gmail.com>
2019-10-07 15:15:37 +0300

    Start resolving failing Causes tests

--- src/containers/Causes/Causes.test.js
+++ src/containers/Causes/Causes.test.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import Causes from './Causes'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { MockedProvider } from '@apollo/react-testing'
+import Causes, { GET_CAUSES_QUERY } from './Causes'
 
 describe('<Causes />', () => {
   let causesWrapper
+  const mocks = [
+    {
+      request: {
+        query: GET_CAUSES_QUERY
+      },
+      result: { data: {
+        causes: [{ name: 'Awesome Cause 1', amountRaised: 50, sponsor: 'unicef' }, { name: 'Awesome Cause 2', amountRaised: 50, sponsor: 'unicef' }]
+      } }
+    }
+  ]
   beforeEach(() => {
-    causesWrapper = shallow(<Causes />)
+    causesWrapper = mount(<MockedProvider mocks={mocks} addTypename={false} cache={new InMemoryCache({ })}><Causes /></MockedProvider>)
   })
 
-  it('contains 1 Trending causes container', () => {
-    expect(causesWrapper.find('TrendingCauses').length).toEqual(1)
+  it.only('contains 1 Trending causes container', async() => {
+    await expect(causesWrapper.find('TrendingCauses').length).toEqual(1)
   })
 
   it('contains 1 Campaigns ending soon container', () => {
