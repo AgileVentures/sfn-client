37e16774cf8be7c7deedda5b600618feaf2baecc
Peter Javorkai <javorkai.peter@gmail.com>
2019-11-15 23:12:54 +0100

    Removed test EmptyCard component

--- src/containers/Causes/CampaignsEndingSoon/CampaignsEndingSoon.js
+++ src/containers/Causes/CampaignsEndingSoon/CampaignsEndingSoon.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 import CauseCard from '../../../components/Causes/CauseCard/CauseCard'
-import EmptyCard from '../../../components/Shared/EmptyCard/EmptyCard'
 
 class CampaignsEndingSoon extends Component {
   render() {
@@ -25,7 +24,6 @@ class CampaignsEndingSoon extends Component {
             daysToGo={45}
             organization="UNICEF"
           />
-          <EmptyCard itemName="Cause" />
         </div>
         <NavLink activeClassName="nav__item--selected" to="/causes?q=ending">
           See all ending causes
