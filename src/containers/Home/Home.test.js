23ac1544334de820577822a9fea9f74bbb578348
Peter Javorkai <javorkai.peter@gmail.com>
2019-11-30 22:18:23 +0100

    Created section for registration flows

--- src/containers/Home/Home.test.js
+++ src/containers/Home/Home.test.js
@@ -17,10 +17,24 @@ describe('<Home />', () => {
   })
 
   it('contains a call to action button', () => {
-    expect(homeWrapper.find('button').length).toEqual(1)
+    expect(homeWrapper.find('.home-container__intro button').length).toEqual(1)
   })
 
   it('contains 3 mood images about artists', () => {
-    expect(homeWrapper.find('img').length).toEqual(3)
+    expect(homeWrapper.find('.home-container__intro img').length).toEqual(3)
+  })
+
+  it('contains a section about how to register', () => {
+    expect(homeWrapper.find('.home-container__registration').length).toEqual(1)
+  })
+
+  it('contains 3 possible registration opportunities', () => {
+    expect(homeWrapper.find('.registration-option-card').length).toEqual(3)
+  })
+
+  it('each of the 3 cards has a button which leads to the registration accordingly', () => {
+    expect(homeWrapper.find('.registration-option-card button').length).toEqual(
+      3
+    )
   })
 })
