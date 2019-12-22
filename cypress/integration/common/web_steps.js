bbcc99d02716cb4a20350f9603dbfd389cd998ac
Peter Javorkai <javorkai.peter@gmail.com>
2019-12-22 21:12:15 +0100

    Fixig cypress failure

--- cypress/integration/common/web_steps.js
+++ cypress/integration/common/web_steps.js
@@ -1,6 +1,6 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 /* global Cypress */
-Cypress.on('window:before:load', (win) => {
+Cypress.on('window:before:load', win => {
   win.gapi = { load: cy.stub().as('load') }
 })
 
@@ -9,11 +9,24 @@ Given('I open Google page', () => {
   cy.visit(url)
 })
 
+/**
+ * Disable css smooth scroll, that doesn't plays nice with cypress.
+ * See https://github.com/cypress-io/cypress/issues/3200
+ */
+const disableSmoothScroll = () => {
+  cy.document().then(document => {
+    const node = document.createElement('style')
+    node.innerHTML = 'html { scroll-behavior: inherit !important; }'
+    document.body.appendChild(node)
+  })
+}
+
 Given('I am on the home page', () => {
   cy.visit('/')
 })
 
 When("I click on 'artist' link", () => {
+  disableSmoothScroll()
   cy.get('.nav-container__left')
     .find('a')
     .contains('Artists')
@@ -21,6 +34,7 @@ When("I click on 'artist' link", () => {
 })
 
 When("I click on 'artist' link on the footer", () => {
+  disableSmoothScroll()
   cy.get('.footer__container')
     .find('nav')
     .find('a')
